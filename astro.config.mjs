f4c53ca58716b608fabec2a98157623bd8b19c51
jonathanj20 <jonathanpossada@gmail.com>
2025-04-13 22:07:00 -0700

    Se agregó la sección de habilidades

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   site: 'https://jonathanj20.github.io',
   base: '/portfolio-web',
+  integrations: [icon()],
 });
